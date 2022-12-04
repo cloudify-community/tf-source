9a87ae945258b7e43dbf67cdc6c39a2d86d36a63
EarthmanT <trammell@cloudify.co>
2022-12-04 11:59:31 -0500

    update token

--- template/modules/public_vm/variables.tf
+++ template/modules/public_vm/variables.tf
@@ -28,6 +28,11 @@ variable "secret_key" {
   description = "Secret key for AWS"
 }
 
+variable "token" {
+  type = string
+  description = "token"
+}
+
 variable "env_name" {
     type = string
     description = "Environment name"
